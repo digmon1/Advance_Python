3ca158becac5f3fca2a4ac5b4009235871301587
sanjay kumar shrestha <xboxfamil639@gmail.com>
2026-07-07 11:23:03 +0545

    git push origin main

--- DATE_Time_module/find_age.py
+++ DATE_Time_module/find_age.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 birthday = datetime(1992,4,25)
 today =datetime.now()
 print(f'person age:{today.year-birthday.year} years')
+age=relativedelta(today, birthday)
+print(f"Age: {age.years} Years, {age.months} Months, {age.days} Days")
